aaf7d2bb1e626ea2d36ed23be9b09af7817abcd3
thepresidentm <miguelanva9@gmail.com>
2021-11-10 20:44:09 -0600

    logica cifrado

--- public/js/validacion.js
+++ public/js/validacion.js
@@ -1,6 +1,7 @@
 const regex = /^[0-9]+$/;
 function validar(){
     if(regex.test(input.value) == true && input.value.length < 4){
+        correrCifrado();
         send();
     }else{
         alert('Solo puedes usar numeros, no puedes enviar un mensaje vacío y la entrada no debe ser mayor a 3 caracteres');
